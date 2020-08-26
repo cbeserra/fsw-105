aae27971d11ab249c90a7772d3c0c659e52a4773
cbeserra <christopher.beserra@bryanuniversity.edu>
2020-08-26 09:53:54 -0500

    first commit

---
+++ Week1/grocery-store-js/index.js
@@ -0,0 +1,9 @@
+var shopper = {
+    name: "Chris",
+    wallet: 50,
+    hasMoney: true,
+    buy: function() {
+        return this.name + "is buying something";
+    }, 
+    groceryCart: ["milk","eggs","bacon", "tortillas"]
+}
