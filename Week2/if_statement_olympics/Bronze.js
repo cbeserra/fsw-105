6e7f83ebb595fc7e7f4b679854ec7874a730bfd7
cbeserra <christopherbeserra@gmail.com>
2020-09-04 22:59:53 -0500

    first commit

--- Week2/if_statement_olympics/Bronze.js
+++ Week2/if_statement_olympics/Bronze.js
@@ -2,19 +2,19 @@ var person = {
     name: "Bobby",
     age: 12
 }
-//Question 1
+//Question-1
 if(person.age >=18){
     console.log(person.name + " is old enough")
-} else {
+}else{
     console.log(person.name + " is not old enough")
 }
-//Question 2
+//Question-2
  if(person.name.startsWith("B")){
     console.log(person.name + " can enter(begins with B)")
-    }else{
+}else{
     console.log(person.name + "cannot enter(does not begin with B)")
 }
-//Question 3
+//Question-3
 if(person.age>=18 && person.name.startsWith("B")){
     console.log(person.name + " may enter(begins with B and older than 18)")
 }else{
---
+++ Week2/if_statement_olympics/silver.js
@@ -0,0 +1,14 @@
+//Question-1
+if(1==='1'){
+    console.log('strict')
+} else if(1==='1'){
+    console.log('loose')
+}else{
+    console.log('not equal at all')
+}
+//Question-2
+if(1<=2 && 2===4){
+    console.log('YES')
+}else{
+    console.log('NO')
+}
